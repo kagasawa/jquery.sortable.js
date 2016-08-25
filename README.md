37fc916e462704d4b21ed8504c35b01d9565b959
kagasawa <kagasawa@web-prom.net>
2016-08-25 17:35:33 +0900

    初期コミット

---
+++ README.md
@@ -0,0 +1,11 @@
+# HTMLの\<table\>内をソートできるjQueryプラグイン #
+
+### 参考 ###
+
+http://qiita.com/rev84/items/23b09fcb27ce218597ad
+
+### 解説 ###
+
+@rev84 氏のjquery.sortable.jsでは文字列のソートが不完全だったので、mergeSortNumber を丸ごと書き換えて日本語でも数値でもソートできるように調整しました。
+
+使い方は全く一緒です。
---
+++ js/jquery.sortable.js
@@ -0,0 +1,113 @@
+;(function($) {
+    $.fn.sortableInit = function(){
+        var elements = this;
+
+        var sortKeyClass = [
+            'sort_number',
+            'sort_number_attr'
+        ];
+
+        elements.each(function(){
+            $(this).find('thead').find('tr:last').find('th').each(function(index){
+                var self = this;
+                // 対象クラスを持たない列にはソートボタンを表示しない
+                if(!sortKeyClass.some(function(v){
+                    return $(self).hasClass(v);
+                })) return this;
+
+                var func = null;
+                var isAttr = null;
+                // 数値型
+                if ($(this).hasClass('sort_number')) {
+                    func = mergeSortNumber;
+                    isAttr = false;
+                }
+                // 属性数値型
+                else if ($(this).hasClass('sort_number_attr')) {
+                    func = mergeSortNumber;
+                    isAttr = true;
+                }
+
+                var ascObj = $('<span>').html('▲').click(function(){
+                    $(this).sortable(index, true, func, isAttr);
+                }).css({"cursor":"pointer"});
+                var descObj = $('<span>').html('▼').click(function(){
+                    $(this).sortable(index, false, func, isAttr);
+                }).css({"cursor":"pointer"});
+                $(this).append($('<span>').addClass('sort_cursor').append(ascObj).append(descObj));
+            });
+        });
+
+        return this;
+    };
+
+    $.fn.sortable = function(index, isAsc, func, isAttr){
+        var element = this;
+
+        var tbody = element.closest('table').find('tbody');
+        // 色付けクラス削除
+        tbody.find('td').removeClass('sort_by_asc sort_by_desc');
+        var trs = [];
+        tbody.find('tr').each(function(i){
+            trs[i] = this;
+        });
+        trs = func(trs, index, isAsc, isAttr);
+        tbody.empty().append(trs);
+        // 色付けクラス追加
+        tbody.find('tr').each(function(){
+            $(this).find('td').eq(index).addClass(isAsc ? 'sort_by_asc' : 'sort_by_desc');
+        });
+        return this;
+    };
+
+    function mergeSortNumber(trs, index, isAsc, isAttr) {
+        
+        var sorter = [];
+
+        // KEY配列だけ収集
+        $.each(trs, function (key){
+            var $tr = $(this).find('td').eq(index);
+            var sort;
+            if ( isAttr ) {
+                sort = $tr.attr('sort-value');
+            } else {
+                sort = $tr.html();
+            }
+            sorter.push({
+                key : key,
+                sort : sort
+            });
+        });
+        
+        // 配列をソート
+        sorter.sort(
+            function(a,b){
+                var aSort = a['sort'];
+                var bSort = b['sort'];
+                
+                if ( !isNaN(aSort) ) {
+                    aSort = parseFloat(aSort);
+                }
+                if ( !isNaN(bSort) ) {
+                    bSort = parseFloat(bSort);
+                }
+                if ( isAsc ) {
+                    if( aSort < bSort ) return -1;
+                    if( aSort > bSort ) return 1;
+                } else {
+                    if( aSort < bSort ) return 1;
+                    if( aSort > bSort ) return -1;
+                }
+                return 0;
+            }
+        );
+        
+        var results = [];
+        $.each(sorter, function (){
+            results.push(trs[this.key]);
+        });
+        
+        return results;
+    }
+
+})(jQuery);
